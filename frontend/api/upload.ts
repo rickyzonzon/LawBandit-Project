65d95556fb75c268e3ffd424ae0c52f36960fac5
Ricky Morri <raptorzonzon@gmail.com>
2025-09-19 19:22:13 -0700

    Debug upload.ts

--- frontend/api/upload.ts
+++ frontend/api/upload.ts
@@ -1,9 +1,9 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import pdfParse from 'pdf-parse';
+// import pdfParse from 'pdf-parse';
 import formidable from 'formidable';
-import fs from 'fs';
-import { extractSchedule } from '../src/extract_schedule.js';
-import { createCalendar } from '../src/calendar.js';
+// import fs from 'fs';
+// import { extractSchedule } from '../src/extract_schedule.js';
+// import { createCalendar } from '../src/calendar.js';
 
 // Built-in Vercel parser can't handle pdfs
 export const config = {
@@ -19,35 +19,49 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     const form = new formidable.IncomingForm({ keepExtensions: true });
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    form.parse(req, async (err: any, _fields: any, files: any) => {
-        console.log("test");
-        try {
-            if (err) throw err;
-            
-            // Make sure a file was uploaded
-            const file = files.syllabus as formidable.File;
-            if (!file) {
-                return res.status(400).json({ error: 'No file uploaded' }) 
-            }
-
-            // Extract text from the pdf
-            const result = await pdfParse(fs.readFileSync(file.filepath));
-            const assignments = await extractSchedule(result.text);
-            
-            // Create the .ics string
-            const calendar = await createCalendar(assignments);
-
-            res.setHeader('Content-Type', 'application/json');
-
-            return res.status(200).json({
-                assignments,
-                calendar: calendar
+    try {
+        const file = await new Promise((resolve, reject) => {
+            form.parse(req, (err, fields, files) => {
+                if (err) return reject(err);
+                resolve({ fields, files});
             });
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        } catch (err: any) {
-            console.error(err);
-            return res.status(500).json({ error: err.message || 'Unknown error' });
-        }
-    });
+        });
+        
+        // console.log(fields);
+        console.log(file);
+    } catch (err) {
+        console.error('Error uploading file: ', err);
+        res.status(500).json({ error: 'Failed to upload file' });
+    }
+    
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // form.parse(req, async (err: any, _fields: any, files: any) => {
+    //     try {
+    //         if (err) throw err;
+            
+    //         // Make sure a file was uploaded
+    //         const file = files.syllabus as formidable.File;
+    //         if (!file) {
+    //             return res.status(400).json({ error: 'No file uploaded' }) 
+    //         }
+
+    //         // Extract text from the pdf
+    //         const result = await pdfParse(fs.readFileSync(file.filepath));
+    //         const assignments = await extractSchedule(result.text);
+            
+    //         // Create the .ics string
+    //         const calendar = await createCalendar(assignments);
+
+    //         res.setHeader('Content-Type', 'application/json');
+
+    //         return res.status(200).json({
+    //             assignments,
+    //             calendar: calendar
+    //         });
+    //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    //     } catch (err: any) {
+    //         console.error(err);
+    //         return res.status(500).json({ error: err.message || 'Unknown error' });
+    //     }
+    // }); 
 }
