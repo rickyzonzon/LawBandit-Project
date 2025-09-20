01997a375a8663367b2e724ea0d39a88d86b394a
Ricky Morri <raptorzonzon@gmail.com>
2025-09-19 21:32:35 -0700

    Update upload.ts

--- frontend/api/upload.ts
+++ frontend/api/upload.ts
@@ -19,20 +19,49 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     const form = formidable({ uploadDir: './public', keepExtensions: true });
 
+    // form.parse(req, (err, file) => {
+    //         if (err) res.status(500).json({ error: `Failed to upload file. ${err}`});
+
+    //         const uploadedFile = file;
+
+    //         if (uploadedFile) {
+    //             //Extract text from the pdf
+    //             const result = pdfParse(uploadedFile.filepath);
+    //             const assignments = extractSchedule(result.text);
+                
+    //             // Create the .ics string
+    //             const calendar = await createCalendar(assignments);
+
+    //             res.setHeader('Content-Type', 'application/json');
+
+    //             return res.status(200).json({
+    //                 assignments,
+    //                 calendar: calendar
+    //             });
+    //         }
+    //         else {
+    //             res.status(500).json({ error: `No file uploaded. ${err}`});
+    //         }
+    // });
+
     try {
-        const file = await new Promise((resolve, reject) => {
-            form.parse(req, (err, file) => {
+        const files = await new Promise((resolve, reject) => {
+            form.parse(req, (err, fields, files) => {
                 if (err) return reject(err);
-                resolve(file);
+                resolve(files);
             });
-        });
+        }) as formidable.Files;
         
-        console.log(file);
+        console.log(files);
+        
+        const pdfFile = files.pdf?.[0];
 
+        if (!pdfFile) return res.status(400).json({ error: 'No PDF detected' });
+        
         res.status(200).json({ message: 'File uploaded. '});
-
+        
         //Extract text from the pdf
-        const result = await pdfParse(fs.readFileSync(file.filepath));
+        const result = await pdfParse(fs.readFileSync(pdfFile.filepath));
         const assignments = await extractSchedule(result.text);
         
         // Create the .ics string
